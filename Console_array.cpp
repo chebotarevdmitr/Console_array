2139e5466b6250667114d51fd53e155bba6e6675
Dmitriy <127180093+chebotarevdmitr@users.noreply.github.com>
2025-05-04 22:23:24 +0300

    fales  serch

--- Console_array.cpp
+++ Console_array.cpp
@@ -1,12 +1,32 @@
 ﻿#include <iostream>
 using namespace std;
 
-int serch
+int serch(int arr[], int val,int size) {
+	int ind = -1;
+	for (int i = 0; i < size; i++)
+	{
+		if (arr[i]==val)
+		{
+			ind = i;
+		}
+	}
+
+	return ind;
+}
 
 int main()
 {
 	cout << "Hello dimchasic " << endl;
-	int arr[10] = { 7,9,4,5,3,6,9,12,10,17 };
-
+	const int SIZE = 10;
+	int arr[SIZE] = { 7,9,4,5,3,6,9,12,10,17 };
+	int index = serch(arr, 5,SIZE);
+	if (index==-1)
+	{
+		cout << "znachenie ne nashli \n";
+	}
+	else
+	{
+		cout << "znachenie nashli " << index << endl;
+	}
 	return 0;
 }
